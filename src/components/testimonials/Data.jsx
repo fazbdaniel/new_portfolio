6eb99ce24c1d7988870c69a54ca20ebff163270d
fazbdaniel <baguma82@gmail.com>
2023-10-12 17:33:44 +0300

    eleventh commit

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,24 @@
+import Image1 from "../../assets/testimonial1.png";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+
+export const Data = [
+    {
+        "id": 1,
+        "image": Image1,
+        "title": "Charmant Bonhome",
+        "description": "A really goo job, especially from a guy who is currently doing so many other things too. two to be exact"
+    },
+    {
+        "id": 2,
+        "image": Image2,
+        "title": "Cecile JTT",
+        "description": "No body understood me, I repeat no body,... except for this man"
+    },
+    {
+        "id": 3,
+        "image": Image3,
+        "title": "Kulondwa Claude",
+        "description": "Cecile, je t'aime trop, ne m'enerve jamais, soit attentif a mes commandement, soit soumise a mes ordre"
+    }
+]
